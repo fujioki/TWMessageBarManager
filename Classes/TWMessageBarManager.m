7a9d06b0872d7c1f2ddf95c637e3bfb829b07b4f
fujioki <dev@nfujii.com>
2014-09-19 15:32:54 +0900

    Fix for iOS8 orientation handling that returns screen frame obediently regardless of status bar  orientation.

--- Classes/TWMessageBarManager.m
+++ Classes/TWMessageBarManager.m
@@ -724,7 +724,9 @@ static UIColor *kTWDefaultMessageBarStyleSheetInfoStrokeColor = nil;
 
 - (CGRect)orientFrame:(CGRect)frame
 {
-    if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation) || UIDeviceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation))
+    BOOL isRunningiOS8OrLater = [[NSProcessInfo processInfo] respondsToSelector:@selector(operatingSystemVersion)];
+    if (!isRunningiOS8OrLater &&
+        (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation) || UIDeviceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)))
     {
         frame = CGRectMake(frame.origin.x, frame.origin.y, frame.size.height, frame.size.width);
     }
